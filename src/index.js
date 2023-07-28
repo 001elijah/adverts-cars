1f501996dd844fb6c9c629675cecf609fcfafde3
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-07-28 12:46:27 +0300

    FIX: gh pages path

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter exact path="/">
+    <BrowserRouter basename="/adverts-cars/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
