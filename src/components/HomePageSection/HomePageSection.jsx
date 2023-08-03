7c79ec123435fe2f52773b06063c8d6205c76492
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-08-03 16:56:37 +0300

    FIX: design

--- src/components/HomePageSection/HomePageSection.jsx
+++ src/components/HomePageSection/HomePageSection.jsx
@@ -4,25 +4,27 @@ import s from './HomePageSection.module.scss';
 
 const HomePageSection = () => {
   return (
-    <section className='container'>
-      <h1 className={s.title}>
-        Adverts Cars car rental service
-      </h1>
-      <h2 className={s.caption}>
-        Rent a car in a matter of seconds, without SMS and registration.
-      </h2>
-      <p className={s.text}>
-        Our car rental service provides a broad selection of cars.
-        Daily or weekend, for a couple or for a whole family.
-        The service meets any of your needs!
-      </p>
-      <Link
-        className={s.button}
-        to={'catalog'}
-        key={'catalog'}
-      >
-        Pick your car
-      </Link>
+    <section className='backgroundImage'>
+      <div className='container'>
+        <h1 className={s.title}>
+          Adverts Cars car rental service
+        </h1>
+        <h2 className={s.caption}>
+          Rent a car in a matter of seconds, without SMS and registration.
+        </h2>
+        <p className={s.text}>
+          Our car rental service provides a broad selection of cars.
+          Daily or weekend, for a couple or for a whole family.
+          The service meets any of your needs!
+        </p>
+        <Link
+          className={s.button}
+          to={'catalog'}
+          key={'catalog'}
+        >
+          Pick your car
+        </Link>
+      </div>
     </section>
   )
 };
